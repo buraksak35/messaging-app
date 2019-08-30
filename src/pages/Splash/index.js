357e08c841379ef71d18fb7fd05112b53ef5cbaa
Burak Sak <buraksakgit@gmail.com>
2019-08-30 17:17:05 +0300

    update on splash

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {View, Text} from 'react-native';
 import {getUserFromLocal} from '../../helpers/auth';
 import {Actions} from 'react-native-router-flux';
 
-export default class Splash extends Component {
+import {onSuccessSession} from '../../store/actions/auth';
+import {styles} from './styles';
+
+class Splash extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      const checkSession = getUserFromLocal();
+    setTimeout(async () => {
+      const checkSession = await getUserFromLocal();
       if (checkSession) {
+        this.props.onSuccessSession(checkSession);
         Actions.chat();
       } else {
         Actions.welcome();
@@ -22,9 +27,14 @@ export default class Splash extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> Splash </Text>
+      <View style={styles.mainContainer}>
+        <Text> Messaing App </Text>
       </View>
     );
   }
 }
+
+export default connect(
+  null,
+  {onSuccessSession},
+)(Splash);
