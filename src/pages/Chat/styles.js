bfb67e014102a3833206d0b04a62682f5865e0f7
Burak Sak <buraksakgit@gmail.com>
2019-08-30 17:16:58 +0300

    Create styles.js

---
+++ src/pages/Chat/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  leaveButton: {
+    position: 'absolute',
+    left: 0,
+    backgroundColor: 'red',
+  },
+});
