42b83b429f5766d9ed89bd2283bbbc62d4ecade0
Burak Sak <buraksakgit@gmail.com>
2019-08-30 19:43:55 +0300

    sorted messages added

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,3 +1,5 @@
+import sortBy from 'lodash.sortby';
+
 const chatDataUrl =
   'https://jsonblob.com/api/jsonBlob/4f421a10-5c4d-11e9-8840-0b16defc864d';
 
@@ -6,7 +8,13 @@ export const getMessagesFromBackend = async () => {
     let messages = await fetch(chatDataUrl);
     let messagesJson = await messages.json();
 
-    return messagesJson;
+    const sortedMessages = sortBy(messagesJson, [
+      function(o) {
+        return o.timestamp;
+      },
+    ]);
+
+    return sortedMessages;
   } catch (error) {
     console.error(error);
   }
