a84cc5823c5a8c35da5ae37f82636fd41d7a843b
Burak Sak <buraksakgit@gmail.com>
2019-08-31 17:52:03 +0300

    Update styles.js

--- src/components/MessageBubble/styles.js
+++ src/components/MessageBubble/styles.js
@@ -8,13 +8,24 @@ export const styles = StyleSheet.create({
   messageTextContainer: {
     borderRadius: 15,
     padding: 14,
+    backgroundColor: '#2980b9',
+  },
+  nameMessageContainer: {
     maxWidth: '60%',
     marginTop: 18,
-    backgroundColor: '#2980b9',
   },
   messageText: {
     color: '#fff',
     fontWeight: '700',
   },
   avatar: { width: 25, height: 25 },
+  ownerMessageContainer: {
+    alignSelf: 'flex-end',
+    flexDirection: 'row-reverse',
+  },
+  nameTxt: {
+    fontSize: 12,
+    color: 'gray',
+    marginBottom: 5,
+  },
 });
