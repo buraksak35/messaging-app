3c0a14cb489e6cde31712971f419512bdc5ebdfd
Burak Sak <buraksakgit@gmail.com>
2019-08-31 17:52:09 +0300

    Update styles.js

--- src/components/GenderPicker/styles.js
+++ src/components/GenderPicker/styles.js
@@ -4,10 +4,12 @@ export const styles = StyleSheet.create({
   mainContainer: {
     flexDirection: 'row',
     backgroundColor: '#bdc3c7',
+    borderRadius: 15,
   },
   buttonContainer: {
     paddingHorizontal: 20,
     paddingVertical: 10,
+    borderRadius: 15,
   },
   txt: {
     fontWeight: '700',
